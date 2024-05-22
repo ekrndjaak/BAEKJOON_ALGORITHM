9cc1b22e216747033a1aeb9ce0daea2bd323f0b9
dlagksrb_00 <dlagksrb0430@naver.com>
2024-05-22 10:09:17 +0900

    0522 Java BaekJoon

---
+++ JAVA/Silver/m10814.java
@@ -0,0 +1,29 @@
+package JAVA.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+public class m10814 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        String[][] arr = new String[N][2];
+        for(int i = 0; i < N; i++){
+            String[] input = br.readLine().split(" ");
+            arr[i][0] = input[0];
+            arr[i][1] = input[1];
+        }
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] a, String[] b) {
+                return Integer.parseInt(a[0]) - Integer.parseInt(b[0]);
+            }
+        });
+        for(int i = 0; i < N; i++){
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
