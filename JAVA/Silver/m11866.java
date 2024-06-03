43ff12e4fcfbb616dd755dd822c046a7a50e3df5
dlagksrb_00 <dlagksrb0430@naver.com>
2024-06-03 14:29:19 +0900

    06-03 Java BaekJoon

--- JAVA/Silver/m11866.java
+++ JAVA/Silver/m11866.java
@@ -1,14 +1,45 @@
+//요세푸스 문제는 다음과 같다.
+//
+//1번부터 N번까지 N명의 사람이 원을 이루면서 앉아있고, 양의 정수 K(≤ N)가 주어진다.
+// 이제 순서대로 K번째 사람을 제거한다. 한 사람이 제거되면 남은 사람들로 이루어진 원을 따라 이 과정을 계속해 나간다.
+// 이 과정은 N명의 사람이 모두 제거될 때까지 계속된다.
+// 원에서 사람들이 제거되는 순서를 (N, K)-요세푸스 순열이라고 한다. 예를 들어 (7, 3)-요세푸스 순열은 <3, 6, 2, 7, 5, 1, 4>이다.
+//
+//N과 K가 주어지면 (N, K)-요세푸스 순열을 구하는 프로그램을 작성하시오.
 package JAVA.Silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class m11866 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // 한줄로 입력받기
 
-        int N = Integer.parseInt(br.readLine());
-
+        String input = br.readLine();
+        String[] inputs = input.split(" ");
+        int N = Integer.parseInt(inputs[0]);
+        int K = Integer.parseInt(inputs[1]);
+        //큐 생성 및 값 대입
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 1; i <= N ; i++){
+            q.add(i);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        while(q.size() != 1){
+            for(int i = 0; i < K-1 ; i++){
+                q.offer(q.poll());
+            }
+            sb.append(q.poll() + ", ");
+        }
+        sb.append(q.poll()+ ">");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
