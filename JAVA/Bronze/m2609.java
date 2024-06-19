c9ac53e349d2dc43d65ec67caf53465335c785b5
dlagksrb_00 <dlagksrb0430@naver.com>
2024-06-19 16:45:31 +0900

    06-19 Java BaekJoon

---
+++ JAVA/Bronze/m2609.java
@@ -0,0 +1,15 @@
+package JAVA.Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class m2609 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        System.out.println(str);
+    }
+}
