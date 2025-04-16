92f4e189449d0730751a0cbd9237c0ff37386438
ekrnd <dlagksrb0430@naver.com>
2025-04-16 19:20:02 +0900

    문제 풀기

--- JAVA/Silver/stack/m10773.java
+++ JAVA/Silver/stack/m10773.java
@@ -1,21 +1,32 @@
 package JAVA.Silver.stack;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
+
 
 public class m10773 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Integer> stack = new Stack<>();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            stack.push(Integer.parseInt(st.nextToken()));
+        Stack<Integer> stack = new Stack<>();
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }else {
+                stack.push(num);
+            }
         }
+        for(int val : stack){
+            sum += val;
+        }
+        System.out.println(sum);
+
     }
 }
