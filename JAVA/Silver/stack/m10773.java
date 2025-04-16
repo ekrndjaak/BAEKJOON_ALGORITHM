aa21ce69bade3aeee2ba6f993da6e49f5412efa2
ekrnd <dlagksrb0430@naver.com>
2025-04-16 19:20:24 +0900

    문제 풀기

--- JAVA/Silver/stack/m10773.java
+++ JAVA/Silver/stack/m10773.java
@@ -7,7 +7,6 @@ import java.io.*;
 public class m10773 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
